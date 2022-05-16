299d8d4d488afeb6c34c1d441ef033f0b54ba82c
Taro Yamashita <taro.ymst@gmail.com>
2022-05-16 09:07:55 +0900

    元号を導出できるようになった。

--- src/Koyomi.FSharp/Koyomi.fs
+++ src/Koyomi.FSharp/Koyomi.fs
@@ -1,1 +1,71 @@
 namespace Koyomi.FSharp
+
+open System
+
+type Epoc =
+    | Reiwa
+    | Heisei
+    | Showa
+
+[<Struct>]
+type Era =
+    { Epoc: Epoc
+      FullName: string
+      Name: string
+      Year: int }
+
+[<RequireQualifiedAccess>]
+module Era =
+    type private Range =
+        | Current of DateTime
+        | Recent of DateTime * DateTime
+
+    let private name =
+        function
+        | Reiwa -> "令和"
+        | Heisei -> "平成"
+        | Showa -> "昭和"
+
+    let private showa =
+        let from = DateTime(1926, 12, 25)
+        let until = DateTime(1989, 1, 7)
+        Recent(from, until)
+
+    let private heisei =
+        let from = DateTime(1989, 1, 8)
+        let until = DateTime(2019, 4, 30)
+        Recent(from, until)
+
+    let private reiwa =
+        let from = DateTime(2019, 5, 1)
+        Current(from)
+
+    let private within (range: Range) (dt: DateTime) =
+        match range with
+        | Recent(from, until) -> from <= dt && dt <= until
+        | Current(from) -> from <= dt
+
+    let private year (dt: DateTime) (ep: Epoc) =
+        match ep with
+        | Reiwa -> dt.Year - 2018
+        | Heisei -> dt.Year - 1988
+        | Showa -> dt.Year - 1925
+
+    let private fullName (dt: DateTime) (ep: Epoc) =
+        let epocYear =
+            match year dt ep with
+            | 1 -> "元"
+            | x -> string x
+        let name' = name ep
+        $"{name'}{epocYear}年"
+
+    let era (dt: DateTime) =
+        let epoc =
+            if within reiwa dt then Reiwa
+            else if within heisei dt then Heisei
+            else if within showa dt then Showa
+            else invalidArg (nameof dt) "昭和以前の元号は利用できません。"
+        { Epoc = epoc
+          FullName = fullName dt epoc
+          Name = name epoc
+          Year = year dt epoc }
