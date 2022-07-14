5f3260acfcac4d8794f63cdd59571a523a38b331
Taro Yamashita <taro.ymst@gmail.com>
2022-07-15 07:52:01 +0900

    2月の祝日を判定できるようになった。

--- tests/Koyomi.FSharp.Tests/Year2022Tests.fs
+++ tests/Koyomi.FSharp.Tests/Year2022Tests.fs
@@ -7,8 +7,8 @@ open FsUnit.Xunit
 
 open Koyomi.FSharp
 
-module January2022Tests =
-    let calendar: obj[] list =
+module Year2022Tests =
+    let january: obj[] list =
         [
             [| DateTime(2022, 1, 1); Some "元日"|]
             [| DateTime(2022, 1, 2); None |]
@@ -44,8 +44,47 @@ module January2022Tests =
         ]
 
     [<Theory>]
-    [<MemberData("calendar")>]
+    [<MemberData("january")>]
     let ``2022年1月の祝日を判定できる`` (dt: DateTime) (expect: string option) =
+        Koyomi.from dt
+        |> Koyomi.holiday
+        |> should equal expect
+
+    let february: obj[] list =
+        [
+            [| DateTime(2022, 2, 1); None |]
+            [| DateTime(2022, 2, 2); None |]
+            [| DateTime(2022, 2, 3); None |]
+            [| DateTime(2022, 2, 4); None |]
+            [| DateTime(2022, 2, 5); None |]
+            [| DateTime(2002, 2, 6); None |]
+            [| DateTime(2022, 2, 7); None |]
+            [| DateTime(2022, 2, 8); None |]
+            [| DateTime(2022, 2, 9); None |]
+            [| DateTime(2022, 2, 10); None |]
+            [| DateTime(2022, 2, 11); Some "建国記念の日" |]
+            [| DateTime(2022, 2, 12); None |]
+            [| DateTime(2022, 2, 13); None |]
+            [| DateTime(2022, 2, 14); None |]
+            [| DateTime(2022, 2, 15); None |]
+            [| DateTime(2022, 2, 16); None |]
+            [| DateTime(2022, 2, 17); None |]
+            [| DateTime(2022, 2, 18); None |]
+            [| DateTime(2022, 2, 19); None |]
+            [| DateTime(2022, 2, 20); None |]
+            [| DateTime(2022, 2, 21); None |]
+            [| DateTime(2022, 2, 22); None |]
+            [| DateTime(2022, 2, 23); None |]
+            [| DateTime(2022, 2, 24); None |]
+            [| DateTime(2022, 2, 25); None |]
+            [| DateTime(2022, 2, 26); None |]
+            [| DateTime(2022, 2, 27); None |]
+            [| DateTime(2022, 2, 28); None |]
+        ]
+
+    [<Theory>]
+    [<MemberData("february")>]
+    let ``2022年2月の祝日を判定できる`` (dt: DateTime) (expect: string option) =
         Koyomi.from dt
         |> Koyomi.holiday
         |> should equal expect
